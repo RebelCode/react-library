c6edabe9333f881a7655a27e0ded2896cf1cc042
Suleiman Mohammed <sumohammed@hotmail.com>
2022-04-17 14:33:39 +0000

    Stable storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,42 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+    // config.module.rules.push({
+    //   test: /\.scss$/,
+    //   use: ["style-loader", "css-loader", "sass-loader"],
+    //   include: path.resolve(__dirname, "../"),
+    // });
+
+    config.module.rules.push({
+      test: /\.pcss$/,
+      use: [
+              "style-loader", 
+              {
+                loader: "css-loader",
+                options: {
+                  modules: {
+                    localIdentName: "[name]__[local]",
+                  },
+                  importLoaders: 1,
+                }
+              }, 
+              "sass-loader"
+            ],
+      include: path.resolve(__dirname, "../")
+    });
+
+    // Return the altered config
+    return config;
+  }
 }
