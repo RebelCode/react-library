da10cacd3d1e760bc10b9ece92e8778934afbb07
Suleiman Mohammed <sumohammed@hotmail.com>
2022-04-26 20:43:16 +0000

    Update rollup config for css modules build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import { babel } from "@rollup/plugin-babel";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import scss from "rollup-plugin-scss";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss-modules";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import replace from "rollup-plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+import cssnano from "cssnano";
+import postcssSimpleVars from "postcss-simple-vars";
 
 export default [
   {
-    input: "./src/index.ts",
+    input: "./index.ts",
     output: [
       {
         file: "dist/index.js",
@@ -20,19 +25,72 @@ export default [
       },
     ],
     plugins: [
-      scss({
-        output: true,
-        failOnError: true,
-        outputStyle: "compressed",
+      nodeResolve({
+        extensions: [".js", ".tsx"],
+      }),
+      replace({
+        sourcemap: false,
+        "process.env.NODE_ENV": JSON.stringify("production"),
       }),
       babel({
+        babelHelpers: "runtime",
+        babelrc: false,
+        sourcemap: false,
         exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: { esmodules: true },
+              bugfixes: true,
+              loose: true,
+            },
+          ],
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+        plugins: [
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-proposal-class-properties",
+          "@babel/plugin-proposal-export-namespace-from",
+          "@babel/plugin-proposal-throw-expressions",
+          "@babel/plugin-transform-runtime",
+        ],
       }),
-      external(),
-      resolve(),
+      postcss({
+        writeDefinitions: true,
+        extract: true,
+        plugins: [
+          autoprefixer(),
+          postcssPresetEnv({ stage: 1 }),
+          postcssSimpleVars(),
+          cssnano({
+            preset: "default",
+          }),
+        ],
+        // Or with custom options for `postcss-modules`
+        modules: true,
+        modules: {},
+      }),
       typescript(),
-      terser(),
+      commonjs({
+        sourceMap: false,
+        include: "node_modules/**",
+        ignoreGlobal: true,
+      }),
+      terser({
+        ecma: 2020,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true,
+          drop_console: true,
+          drop_debugger: true,
+        },
+        output: { quote_style: 1 },
+      }),
     ],
+    external: ["react", "react-dom"],
   },
 ];
